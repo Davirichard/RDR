edabecb2a516747101b50edf585c91354ee78d64
David <programadorDev18@gmail.com>
2022-08-19 15:41:25 -0300

    RDR

---
+++ js/question.js
@@ -0,0 +1,14 @@
+
+const faq = document.getElementsByClassName('faq-page')
+let i
+for (i = 0; i < faq.length; i++) {
+  faq[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+    const body = this.nextElementSibling
+    if (body.style.display === 'block') {
+      body.style.display = 'none'
+    } else {
+      body.style.display = 'block'
+    }
+  })
+}
